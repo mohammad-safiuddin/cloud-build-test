858a252aa0a27ff60d67f267eb18e4aaf2e66be9
Safiuddin Mohammad <T965981@corp.ads>
2021-10-28 15:41:52 -0400

    Test Slack

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Testing Cloud Build Slack</h1>
+      <h1>Testing Cloud Build Slack Notifications Test</h1>
     </div>
   );
 }
