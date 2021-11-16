5e5665c4d418bbcd10cf80f3f40f656f9f6c696d
Safiuddin Mohammad <T965981@corp.ads>
2021-11-16 15:27:17 -0500

    test secret manager

---
+++ src/SecretManager.js
@@ -0,0 +1,46 @@
+// Import the Secret Manager client and instantiate it:
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+const client = new SecretManagerServiceClient();
+
+
+/**
+ * TODO(developer): Uncomment these variables before running the sample.
+ */
+var parent = 'projects/215735978261'; // Project for which to manage secrets.
+var secretId = 'foo'; // Secret ID.
+var payload = 'hello world!'; // String source data.
+
+async function createAndAccessSecret() {
+  // Create the secret with automation replication.
+  const [secret] = await client.createSecret({
+    parent: parent,
+    secret: {
+      name: secretId,
+      replication: {
+        automatic: {},
+      },
+    },
+    secretId,
+  });
+
+  console.info(`Created secret ${secret.name}`);
+
+  // Add a version with a payload onto the secret.
+  const [version] = await client.addSecretVersion({
+    parent: secret.name,
+    payload: {
+      data: Buffer.from(payload, 'utf8'),
+    },
+  });
+
+  console.info(`Added secret version ${version.name}`);
+
+  // Access the secret.
+  const [accessResponse] = await client.accessSecretVersion({
+    name: version.name,
+  });
+
+  const responsePayload = accessResponse.payload.data.toString('utf8');
+  console.info(`Payload: ${responsePayload}`);
+}
+createAndAccessSecret();
